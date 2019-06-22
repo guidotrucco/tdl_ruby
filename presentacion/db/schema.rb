1413546647cf6d1c9d7ddeb95b2927c8212813e6
Hernan Tain <hernantain@gmail.com>
2019-06-22 16:25:44 -0300

    Student class added

--- presentacion/db/schema.rb
+++ presentacion/db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_230528) do
+ActiveRecord::Schema.define(version: 2019_06_22_192216) do
 
   create_table "professors", force: :cascade do |t|
     t.string "name"
     t.integer "file_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.integer "padron"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
