4e5e1e0ee69d160f83cfbe5bb35ec67ee4cd4319
Juan Pablo Strnisko <jpstrnisko@gmail.com>
2019-06-23 19:36:07 -0300

    test de validaciones

--- university/test/models/professor_test.rb
+++ university/test/models/professor_test.rb
@@ -12,10 +12,19 @@ class ProfessorTest < ActiveSupport::TestCase
     assert_not professor.save()
   end
 
+
   test "professor sin numero no graba" do
     professor = Professor.new({ :name => "Profesor" })
     assert_not professor.save()
   end
+
+  test "numero debe ser mas largo que 4 caracteres" do
+    professor = Professor.new({ :name => "Profesor", :file_number => "123" })
+    assert_not professor.save()
+    professor = Professor.new({ :name => "Profesor", :file_number => "12345" })
+    assert professor.save()
+  end
+
 
   test "professor con todos los parametros graba" do
     professor = Professor.new({ :name => "Alumno", :file_number => "12345" })
--- university/test/models/student_test.rb
+++ university/test/models/student_test.rb
@@ -17,6 +17,14 @@ class StudentTest < ActiveSupport::TestCase
     assert_not student.save()
   end
 
+  test "padron debe ser mas largo que 4 caracteres" do
+    student = Student.new({ :name => "Alumno", :padron => "123", :email => "alumno@mail.com" })
+    assert_not student.save()
+    student = Student.new({ :name => "Alumno", :padron => "12345", :email => "alumno@mail.com" })
+    assert student.save()
+  end
+
+
   test "student sin mail valido no graba" do
     student = Student.new({ :name => "Alumno", :padron => "12345", :email => "mailfalso" })
     assert_not student.save()
