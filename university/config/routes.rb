a4a5475511a4c3258fe381dc6fc12724678a72ec
Hernan Tain <hernantain@gmail.com>
2019-06-22 22:28:04 -0300

    New homepage

--- university/config/routes.rb
+++ university/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   resources :students
   resources :subjects
   resources :professors
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'students/:id/enroll', to: 'students#enroll'
   post 'subjects/:id/add_student', to: 'subjects#add_student'
